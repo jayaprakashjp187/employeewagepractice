c1b925519680e47ba3598bc46f7b25f16de2b724
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-09 15:03:30 +0530

    if part time employee is present

--- employeeWagePractice/Program.cs
+++ employeeWagePractice/Program.cs
@@ -6,22 +6,28 @@
         {
             Console.WriteLine("welcomes to the employeeWage calculations !");
             const int Full_Time= 1;
+            const int Part_Time = 2;
             int empWage = 0;
             const int Rate_Per_Hour = 20;
             int empHrs = 0;
             Random random = new Random();
-            int randomInput=random.Next(0,2);
+            int randomInput=random.Next(0,3);
             if(Full_Time==randomInput)
             {
                 empHrs = 8;
                 Console.WriteLine("employee is Present");
                
             }
+            else if(Part_Time==randomInput)
+            {
+                empHrs = 4;
+                Console.WriteLine("part time employee is present");
+                
+            }
             else
             {
-                
                 Console.WriteLine("employee is absent");
-                
+
             }
             empWage = empHrs*Rate_Per_Hour;
             Console.WriteLine(empWage);
