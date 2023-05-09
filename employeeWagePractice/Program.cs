4983d70f4199d06e4e16f4d9dbecdd2cd51c6400
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-09 14:01:59 +0530

    calculating employee wage

--- employeeWagePractice/Program.cs
+++ employeeWagePractice/Program.cs
@@ -4,18 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            const int Is_Present = 1;
+            Console.WriteLine("welcomes to the employeeWage calculations !");
+            const int Full_Time= 1;
+            int empWage = 0;
+            const int Rate_Per_Hour = 20;
+            int empHrs = 0;
             Random random = new Random();
             int randomInput=random.Next(0,2);
-            if(Is_Present==randomInput)
+            if(Full_Time==randomInput)
             {
+                empHrs = 8;
                 Console.WriteLine("employee is Present");
+               
             }
             else
             {
+                
                 Console.WriteLine("employee is absent");
+                
             }
+            empWage = empHrs*Rate_Per_Hour;
+            Console.WriteLine(empWage);
+            
+
         }
 
     }
