1801e9b382336c695aeaf85241f47044cc7cc9fb
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-09 20:09:10 +0530

    using for loop calculate the employeeWage

--- employeeWagePractice/Program.cs
+++ employeeWagePractice/Program.cs
@@ -9,19 +9,27 @@
             const int Part_Time = 2;
             int empWage = 0;
             const int Rate_Per_Hour = 20;
+            const int EmployeeWagesPerMonth = 20;
+            int totalEmployeeWage = 0;
             int empHrs = 0;
             Random random = new Random();
             int randomInput=random.Next(0,3);
-            switch(randomInput)
+            for (int day = 1; day <= EmployeeWagesPerMonth; day++)
             {
-                case Full_Time: empHrs = 8; break;
-                case Part_Time: empHrs = 4; break;
-                default: empHrs = 0; break;
+                switch (randomInput)
+                {
+                    case Full_Time: empHrs = 8;Console.WriteLine("full time employee is present"); break;
+                    case Part_Time: empHrs = 4; Console.WriteLine("part time employee is present"); break;
+                    default: empHrs = 0; Console.WriteLine("employee is absent"); break;
 
+                }
+
+                empWage = empHrs * Rate_Per_Hour;
+                Console.WriteLine("Employee Wage  Day {0}: {1}", day, empWage);
+                totalEmployeeWage += empWage;
             }
-       
-            empWage = empHrs*Rate_Per_Hour;
-            Console.WriteLine(empWage);
+            
+            Console.WriteLine("total employee wage per month="+totalEmployeeWage);
             
 
         }
