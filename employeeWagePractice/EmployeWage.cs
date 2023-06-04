d01c411c59c08c322b1f27c6673450713ed50450
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-04 21:51:23 +0530

    without passing parameters using instance variable calculating employee wage for multiple companies

--- employeeWagePractice/EmployeWage.cs
+++ employeeWagePractice/EmployeWage.cs
@@ -9,44 +9,81 @@ namespace employeeWagePractice
     internal class EmployeWage
     {
 
-        //public string CompanyName { get; set; }
-        //public int ratePerHour { get; set; }
-        //public int maxWorkingDaysInAmonth { get; set; }
-        //public int maxWorkingHours { get; set; }
-         
-        public static void CalculateEmployeeWage(EmployeeDetails employee)
+        public string CompanyName { get; set; }
+        public int ratePerHour { get; set; }
+        public int maxWorkingDaysInAmonth { get; set; }
+        public int maxWorkingHours { get; set; }
+        public int totalEmployeeWage = 0;
+        public int totalWorkingHours = 0;
+        public int day = 1;
+        public int empWage = 0;
+        public int empHrs = 0;
+
+        public static void CalculateEmployeeWage()
           {
                 const int Full_Time = 1;
                 const int Part_Time = 2;
-                
+            EmployeWage e1 = new EmployeWage { CompanyName = "Infosys", ratePerHour = 30, maxWorkingDaysInAmonth = 22, maxWorkingHours = 160 };
+            EmployeWage e2 = new EmployeWage { CompanyName = "TCS", ratePerHour = 32, maxWorkingDaysInAmonth = 22, maxWorkingHours = 160 };
+            EmployeWage e3 = new EmployeWage { CompanyName = "HCL", ratePerHour = 28, maxWorkingDaysInAmonth = 22, maxWorkingHours = 160 };
 
-                int totalEmployeeWage = 0;
-                int totalWorkingHours = 0;
-                int day = 1;
-                int empWage = 0;
-                int empHrs = 0;
+               
                 Random random = new Random();
                 int randomInput = random.Next(0, 3);
-                while (day <= employee.maxWorkingDaysInAmonth && totalWorkingHours < employee.maxWorkingHours)
+                while (e1.day <= e1.maxWorkingDaysInAmonth && e1.totalWorkingHours < e1.maxWorkingHours)
                 {
                     switch (randomInput)
                     {
-                        case Full_Time: empHrs = 8; Console.WriteLine("full time employee is present"); break;
-                        case Part_Time: empHrs = 4; Console.WriteLine("part time employee is present"); break;
-                        default: empHrs = 0; Console.WriteLine("employee is absent"); break;
+                        case Full_Time: e1.empHrs = 8; Console.WriteLine("full time employee is present"); break;
+                        case Part_Time: e1.empHrs = 4; Console.WriteLine("part time employee is present"); break;
+                        default: e1.empHrs = 0; Console.WriteLine("employee is absent"); break;
 
                     }
 
-                    empWage = empHrs * employee.ratePerHour;
+                    e1.empWage = e1.empHrs * e1.ratePerHour;
                    
-                    totalEmployeeWage += empWage;
-                    totalWorkingHours += empHrs;
-                    day++;
+                    e1.totalEmployeeWage += e1.empWage;
+                    e1.totalWorkingHours += e1.empHrs;
+                    e1.day++;
                 }
-            Console.WriteLine(employee.CompanyName + "is" + totalEmployeeWage);
+            Console.WriteLine(e1.CompanyName + "is" + e1.totalEmployeeWage);
+            while (e2.day <= e2.maxWorkingDaysInAmonth && e2.totalWorkingHours < e2.maxWorkingHours)
+            {
+                switch (randomInput)
+                {
+                    case Full_Time: e2.empHrs = 8; Console.WriteLine("full time employee is present"); break;
+                    case Part_Time: e2.empHrs = 4; Console.WriteLine("part time employee is present"); break;
+                    default: e2.empHrs = 0; Console.WriteLine("employee is absent"); break;
 
-               
-            
+                }
+
+                e2.empWage = e2.empHrs * e2.ratePerHour;
+
+                e2.totalEmployeeWage += e2.empWage;
+                e2.totalWorkingHours += e2.empHrs;
+                e2.day++;
+            }
+            Console.WriteLine(e2.CompanyName + "is" + e2.totalEmployeeWage);
+            while (e3.day <= e3.maxWorkingDaysInAmonth && e3.totalWorkingHours < e3.maxWorkingHours)
+            {
+                switch (randomInput)
+                {
+                    case Full_Time: e3.empHrs = 8; Console.WriteLine("full time employee is present"); break;
+                    case Part_Time: e3.empHrs = 4; Console.WriteLine("part time employee is present"); break;
+                    default: e3.empHrs = 0; Console.WriteLine("employee is absent"); break;
+
+                }
+
+                e3.empWage = e3.empHrs * e3.ratePerHour;
+
+                e3.totalEmployeeWage += e3.empWage;
+                e3.totalWorkingHours += e3.empHrs;
+                e3.day++;
+            }
+            Console.WriteLine(e3.CompanyName + "is" + e3.totalEmployeeWage);
+
+
+
 
         }
 
--- employeeWagePractice/Program.cs
+++ employeeWagePractice/Program.cs
@@ -6,14 +6,12 @@
         {
             Console.WriteLine("welcomes to the employeeWage calculations !");
             
-            List<EmployeeDetails> e=new List<EmployeeDetails> ();
-            EmployeeDetails e1=new EmployeeDetails { CompanyName="Infosys",ratePerHour=30,maxWorkingDaysInAmonth=22,maxWorkingHours=160 };
-            EmployeeDetails e2 = new EmployeeDetails { CompanyName = "TCS", ratePerHour = 32, maxWorkingDaysInAmonth = 22, maxWorkingHours = 160 };
-            EmployeeDetails e3 = new EmployeeDetails { CompanyName = "HCL", ratePerHour = 28, maxWorkingDaysInAmonth = 22, maxWorkingHours = 160 };
+            List<EmployeWage> e=new List<EmployeWage> ();
+           
 
-            EmployeWage.CalculateEmployeeWage(e1);
-            EmployeWage.CalculateEmployeeWage(e2);
-            EmployeWage.CalculateEmployeeWage(e3);
+            EmployeWage.CalculateEmployeeWage();
+           // EmployeWage.CalculateEmployeeWage();
+           // EmployeWage.CalculateEmployeeWage();
 
         }
 
